ae10c1202432c2b1db2806fc212df6c4a4e26402
Sharon Mwenda <smkathambi@gmail.com>
2022-10-10 11:59:24 +0300

    updated sessions contoller with create and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,12 @@ class UsersController < ApplicationController
     end
 
     def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else
+            render json: {error: 'Not authorized'}, status: :unauthorized
+        end
     end
 
     private
